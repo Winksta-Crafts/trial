9d5c035441c56e24096692aa6c95d0411073ddf8
Winksta <winkstacrafts@gmail.com>
2025-05-12 15:48:23 +0300

    Add files via upload

--- divmajor.js
+++ divmajor.js
@@ -1,6 +1,6 @@
 const divmajorData=`
     <div class="video">
-        <a href="https://drive.google.com/uc?export=download&id=1jMg84nyXdFdirK8crXB_6yfd_GdaNzTs"> <img src="https://github.com/Winksta-Crafts/trial/blob/main/picsss/dp.PNG" id="picture"/></a>
+        <a href="https://drive.google.com/uc?export=download&id=1jMg84nyXdFdirK8crXB_6yfd_GdaNzTs"> <img src="/picsss/dp.PNG" id="picture"/></a>
     </div>
     
     <div class="video">
